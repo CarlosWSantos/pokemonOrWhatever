8550810f5fcb521044de5a3f77983ce456fd2cb5
Willian <51964910+CarlosWSantos@users.noreply.github.com>
2023-05-11 21:10:39 -0300

    Add files via upload

---
+++ img/sprites and sheets/tile-archives/Game Boy Advance - Pokemon FireRed LeafGreen - Pallet Town.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Game Boy Advance - Pokemon FireRed LeafGreen - Pallet Town" tilewidth="16" tileheight="16" tilecount="480" columns="24">
+ <image source="../Game Boy Advance - Pokemon FireRed LeafGreen - Pallet Town.png" width="384" height="320"/>
+</tileset>
